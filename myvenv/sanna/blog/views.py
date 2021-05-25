056c9cf932317d3bf42ccfa77eeb243e1844703e
Matias Gallardo Oliver <matias.04@hotmail.es>
2021-05-25 16:46:25 -0400

    Añadido el inicio para views

--- myvenv/sanna/blog/views.py
+++ myvenv/sanna/blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse, redirect, render
+from django.views.generic.edit import FormView
+
+#Importado de modelos que aún no existen, solo son un ejemplo.
+#from blog.forms import CreateUserForm, ClienteForm, ProductForm
+from blog.models import *
+from django import forms
+
+#Imports para el manejo de usuarios
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 
 # Create your views here.
+# <----- REDIRECCIONES POR CARPETA ----->
+# <----- PRINCIPAL ----->
+def inicio(request):
+    return render(request, 'Principal/inicio.html', {'title':'index'})
+
+# <=============================================>
+
+
+# <----- CREDENCIALES ----->
+
+
+# <=============================================>
+
+
+# <----- LISTAMEDICAMENTOS ----->
+
+
+# <=============================================>
+
+
+# <----- PERFILES ----->
+
+
+# <=============================================>
+
+
+# <----- RESET_PASSWORD ----->
+
+
+# <=============================================>
