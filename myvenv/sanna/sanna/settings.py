25d6733e6210487f9d33b5d879ab8ec608239b4c
Dannfer <danjesus320@gmail.com>
2021-05-25 21:23:41 -0400

    connection to mysql - xampp

--- myvenv/sanna/sanna/settings.py
+++ myvenv/sanna/sanna/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'sanna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'med',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306', #my port is 3306
     }
 }
 
