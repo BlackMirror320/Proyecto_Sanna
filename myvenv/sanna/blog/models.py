fe730e942560040365a494836d2edb467ca99a69
Dannfer <danjesus320@gmail.com>
2021-05-27 19:36:20 -0400

    r

--- myvenv/sanna/blog/models.py
+++ myvenv/sanna/blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.db.models.fields import CharField
 from django.db.models.fields import IntegerField
 from django.utils import timezone
+from django.db import connections
 
 class User(models.Model):
         id=models.AutoField(primary_key=True)
-        nombre=models.CharField(max_length=20)
-        rut=models.CharField(max_length=20)
-        mail=models.CharField(max_length=20)
+        nombre=models.CharField(max_length=30)
+        rut=models.CharField(max_length=9)
+        mail=models.CharField(max_length=30)
         nombre=models.CharField(max_length=20)
         fono=models.IntegerField
         passwd=models.CharField(max_length=20)
